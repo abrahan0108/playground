203c8419a576a81365de0bbbfa05712aa0c9e9a0
Abrahan Cortés Hernández <abrahan@localhost.localdomain>
2019-10-10 18:16:41 -0500

    Añado funcionalidad de perfil

--- registration/forms.py
+++ registration/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class UserCrationFormWithEmail(UserCreationForm):
@@ -16,5 +17,37 @@ class UserCrationFormWithEmail(UserCreationForm):
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError("El email ya está registrado, \
                                         prueba con otro")
+        return email
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio', 'link']
+        widgets = {
+            'avatar': forms.ClearableFileInput(
+                attrs={'class':'form-control-file mt-3'}),
+            'bio': forms.Textarea(
+                attrs={'class':'form-control mt-3', 'rows':3,
+                       'placeholder':'Biografia'}),
+            'link':forms.URLInput(
+                attrs={'class':'form-control mt-3', 'placeholder':'Enlace'}),
+        }
+
+
+class EmailForm(forms.ModelForm):
+    email = forms.EmailField(required=True, help_text="Requerido, 254 \
+                             caracteres como máximo y debe ser válido")
+
+    class Meta:
+        model = User
+        fields =['email']
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if 'email' in self.changed_data:
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError("El email ya está registrado, \
+                                            prueba con otro")
         return email
 
--- registration/views.py
+++ registration/views.py
@@ -1,7 +1,11 @@
-from .forms import UserCrationFormWithEmail 
+from .forms import UserCrationFormWithEmail, ProfileForm, EmailForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django import forms
+from django.views.generic.edit import UpdateView 
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 
 # Create your views here.
@@ -31,4 +35,36 @@ class SignUpView(CreateView):
                    'placeholder':'Repite la contraseña'})
 
         return form
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfileUpdate(UpdateView):
+    form_class = ProfileForm
+    success_url = reverse_lazy('profile')
+    template_name = 'registration/profile_form.html'
+
+    def get_object(self):
+        # Recupera el objeto que se va a editar
+        profile, created = Profile.objects.get_or_create(user=self.request.user)
+        return profile
+
+
+@method_decorator(login_required, name='dispatch')
+class EmailUpdate(UpdateView):
+    form_class = EmailForm 
+    success_url = reverse_lazy('profile')
+    template_name = 'registration/profile_email_form.html'
+
+    def get_object(self):
+        # Recupera el objeto que se va a editar
+        return self.request.user 
+
+    def get_form(self, form_class=None):
+        form = super(EmailUpdate, self).get_form()
+        # Se modifica en tiempo real
+        form.fields['email'].widget = forms.EmailInput(
+            attrs={'class':'form-control mb-2',
+                   'placeholder':'Email'})
+        return form
+
 
